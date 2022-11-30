856438d32bb2343b1b715bfb31956ad4f368f79c
Brent Hopkins <brentph@email.sc.edu>
2022-11-30 13:28:17 -0500

    saving work

--- courseProject/src/Hopkins_Brent_QuickSort.cpp
+++ courseProject/src/Hopkins_Brent_QuickSort.cpp
@@ -15,7 +15,14 @@ public:
     int count;
     double time;
 };
-
+/**
+ * @brief HoarePartiton
+ *
+ * @param arr the vector of doubles being partitioned
+ * @param low the start index of the vector
+ * @param high the end index of the vector
+ * @return int the index of the partition
+ */
 int HoarePartition(vector<double> &arr, int low, int high)
 {
     double pivot = arr[low];
@@ -43,16 +50,30 @@ int HoarePartition(vector<double> &arr, int low, int high)
         swap(arr[i], arr[j]);
     }
 }
+/**
+ * @brief picks a random index between low and high and swaps it with low
+ *
+ * @param arr a vector of doubles
+ * @param low the start index of the vector
+ * @param high the end index of the vector
+ * @return int the partition found using the random pivot value
+ */
 int partition_r(vector<double> &arr, int low, int high)
 {
-    // Generate a random number in between
-    // low .. high
+
     srand(time(NULL));
     int random = low + rand() % (high - low);
     swap(arr[random], arr[low]);
     return HoarePartition(arr, low, high);
 }
 
+/**
+ * @brief randomized quicksort
+ *
+ * @param arr the vector of doubles being sorted
+ * @param low the starting index of the vector
+ * @param high the end index of the vector
+ */
 void QuickSort(vector<double> &arr, int low, int high)
 {
     if (low < high)
@@ -62,6 +83,14 @@ void QuickSort(vector<double> &arr, int low, int high)
         QuickSort(arr, s + 1, high);
     }
 }
+/**
+ * @brief tracks the execution time of quicksort
+ *
+ * @param arr the vector of doubles being sorted
+ * @param low the start index of the vector
+ * @param high the end index of the vector
+ * @return double the execution time in ms
+ */
 double TimedQuickSort(vector<double> &arr, int low, int high)
 {
     auto start = high_resolution_clock::now();
@@ -71,6 +100,12 @@ double TimedQuickSort(vector<double> &arr, int low, int high)
     return duration.count();
 }
 
+/**
+ * @brief reads in double values from a given input file
+ *
+ * @param filename the file being read
+ * @return vector<double> the double values from the input file
+ */
 vector<double> readFile(string filename)
 {
     vector<double> rv;
@@ -96,7 +131,12 @@ vector<double> readFile(string filename)
     input.close();
     return rv;
 }
-
+/**
+ * @brief writes to a file
+ *
+ * @param outputfile the file being written to
+ * @param outString the string being written to the file
+ */
 void writetoFile(string outputfile, string outString)
 {
     ofstream output(outputfile);
@@ -110,7 +150,13 @@ void writetoFile(string outputfile, string outString)
     }
     output.close();
 }
-
+/**
+ * @brief generates a string containing the values in arr and the execution time
+ *
+ * @param arr the values being placed in the string
+ * @param executionTime the execution time being placed in the string
+ * @return string contains the values from arr and the time taken to sort them
+ */
 string GenerateSortedString(vector<double> &arr, double executionTime)
 {
     string out = to_string(arr[0]);
@@ -123,7 +169,12 @@ string GenerateSortedString(vector<double> &arr, double executionTime)
     out = out + "    Execution Time:" + to_string(executionTime);
     return out;
 }
-
+/**
+ * @brief generates a string containing the input size and execution time for each result in the provided vector
+ *
+ * @param arr the results being converted to a string representation
+ * @return string contains the data from every result in arr
+ */
 string GenerateExecutionTimeString(vector<result> arr)
 {
     string out = "";
@@ -133,7 +184,12 @@ string GenerateExecutionTimeString(vector<result> arr)
     }
     return out;
 }
-
+/**
+ * @brief avgs the exection times for a vector of results and returns the result as a string
+ *
+ * @param arr the results being averaged
+ * @return string the average of the results in arr
+ */
 string GenerateExecutionAverageString(vector<result> arr)
 {
     double avg;
@@ -145,7 +201,12 @@ string GenerateExecutionAverageString(vector<result> arr)
     avg = avg / arr.size();
     return to_string(avg);
 }
-
+/**
+ * @brief applies quicksort to the 25 input files generated for the specified input size
+ *
+ * @param arr a vector of results for each input file
+ * @param inputsize the input size for the files being processed
+ */
 void QuickSortInputFiles(vector<result> &arr, string inputsize)
 {
     for (int i = 0; i < 25; i++)
@@ -161,7 +222,7 @@ void QuickSortInputFiles(vector<result> &arr, string inputsize)
 }
 
 /**
- * @brief
+ * @brief applies quick sort to the files created by the input file generator and outputs execution time stats
  *
  */
 void BulkSort()
@@ -191,8 +252,8 @@ int main(int argc, char *argv[])
     if (argc == 3)
     {
         vector<double> arr;
-        string input = "/home/oingoboingo/code/c++/CSCE350/courseProject/data/Input_1000_0.txt";
-        string outPut = "/home/oingoboingo/code/c++/CSCE350/courseProject/data/output1000_0.txt";
+        string input = argv[1];
+        string outPut = argv[2];
         double exeTime;
         arr = readFile(input);
         exeTime = TimedQuickSort(arr, 0, arr.size() - 1);
